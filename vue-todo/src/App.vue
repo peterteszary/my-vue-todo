671349484f5201cf2f8531c9fdd19aaf5c3b451a
peterteszary <peterteszary@gmail.com>
2024-01-01 19:33:45 +0100

    test for user privileges couse I coulnd not save or create a file

--- vue-todo/src/App.vue
+++ vue-todo/src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
+  <div id="app" class="container">
+    <img src="./assets/logo.png" class="logo">
     <router-view/>
   </div>
 </template>
@@ -12,12 +12,28 @@ export default {
 </script>
 
 <style>
+
+*{
+  box-sizing: border-box;
+}
+
+.container {
+  max-width: 600px;
+  margin: 0 auto;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /*text-align: center;*/
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.logo {
+  display: block;
+  margin: 20px auto;
+  height: 75px;
 }
 </style>
