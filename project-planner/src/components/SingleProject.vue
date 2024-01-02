921a05c2c147673e0dd4aee6d382c469f8b83064
Teszáry Péter <peterteszary@gmail.com>
2024-01-02 15:03:27 +0100

    AddProject.vue is created

--- project-planner/src/components/SingleProject.vue
+++ project-planner/src/components/SingleProject.vue
@@ -1,11 +1,11 @@
 <template>
-    <div class="project">
+    <div class="project" :class="{ complete: project.complete }">
     <div class="actions">
         <h3 @click="showDetails = !showDetails">{{ project.title }}</h3>
         <div class="icons">
             <span class="material-icons">edit</span>
             <span @click="deleteProject" class="material-icons">delete</span>
-            <span @click="toggleComplete" class="material-icons">done</span>
+            <span @click="toggleComplete" class="material-icons tick">done</span>
         </div>
     </div>
     <div v-if="showDetails" class="details">
@@ -35,7 +35,9 @@ export default{
                 method: 'PATCH',
                 headers: {'Content-Type': 'application/json'},
                 body: JSON.stringify({ complete: !this.project.complete}) 
-            })
+            }).then(() => {
+                this.$emit('complete', this.project.id)
+            }).catch((err) => console.log(err))
         }
     }
 }
@@ -69,6 +71,14 @@ h3{
 
 .material-icons:hover {
     color: #777
+}
+/* completed projects */
+.project.complete {
+    border-left:  4px solid #00ce89;
+}
+
+.project.complete .tick {
+    color: #00ce89;   
 }
 
 </style>
