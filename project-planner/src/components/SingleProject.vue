0a3c0a2fafc5ddea41ac53f2be2b39faa0549265
Teszáry Péter <peterteszary@gmail.com>
2024-01-02 14:22:07 +0100

    Patch Request for completetion of tasks.

--- project-planner/src/components/SingleProject.vue
+++ project-planner/src/components/SingleProject.vue
@@ -2,6 +2,11 @@
     <div class="project">
     <div class="actions">
         <h3 @click="showDetails = !showDetails">{{ project.title }}</h3>
+        <div class="icons">
+            <span class="material-icons">edit</span>
+            <span @click="deleteProject" class="material-icons">delete</span>
+            <span @click="toggleComplete" class="material-icons">done</span>
+        </div>
     </div>
     <div v-if="showDetails" class="details">
         <p>{{ project.details }}</p>
@@ -15,7 +20,22 @@ export default{
     props: ['project'],
     data() {
         return {
-            showDetails: false
+            showDetails: false,
+            uri:  'http://localhost:3000/projects' + this.project.id
+        }
+    },
+    methods: {
+        deleteProject() {
+            fetch(this.uri, {method: 'DELETE' })
+                .then(() => this.$emit('delete', this.project.id))
+                .catch(err => console.log(err))
+        },
+        toggleComplete() {
+            fetch(this.uri, { 
+                method: 'PATCH',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({ complete: !this.project.complete}) 
+            })
         }
     }
 }
@@ -34,6 +54,21 @@ export default{
 h3{
     cursor: pointer;
 }
+.actions {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
 
+.material-icons {
+    font-size: 24px;
+    margin-left: 10px;
+    color: #bbb;
+    cursor: pointer;
+}
+
+.material-icons:hover {
+    color: #777
+}
 
 </style>
